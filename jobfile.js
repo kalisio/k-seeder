293afc651e73a05ddc2c1bb1e0f760fe50772f6c
Christophe Nouguier <christophe.nouguier@free.fr>
2019-01-03 10:54:08 +0100

    WIP #4

--- jobfile.js
+++ jobfile.js
@@ -65,8 +65,8 @@ module.exports = {
             ContainerSpec: {
               Image: 'kalisio/k-seeder:mapproxy-seed-latest',
               Env: [
-                'AWS_ACCESS_KEY_ID=' + process.env.S3_ACCESS_KEY,
-                'AWS_SECRET_ACCESS_KEY=' + process.env.S3_SECRET_ACCESS_KEY
+                'AWS_ACCESS_KEY_ID=' + process.env.AWS_ACCESS_KEY,
+                'AWS_SECRET_ACCESS_KEY=' + process.env.AWS_SECRET_ACCESS_KEY
               ],
               Mounts: [
                 {
